d959778f5010fc43ee9dd65594840efca6f4bb16
Andrey Popp <8mayday@gmail.com>
2014-02-24 03:07:50 +0400

    Better animations

--- index.js
+++ index.js
@@ -24,8 +24,25 @@ var MainPage = React.createClass({
   render: function() {
     return (
       <div className="MainPage Page">
-        <h1>Main page</h1>
-        Go to <Link href="/about">about page</Link>.
+        <div className="Page__wrapper">
+          <h1>Main page</h1>
+          <p>
+            This demo shows how to do <a href={githubHref}>animated page
+            transitions</a> with <a href={rrcHref}>React Router component</a>.
+          </p>
+          <p>
+            All you need is to use <code>AnimatedLocations</code> router:
+          </p>
+          <pre>{[
+            '<AnimatedLocations className="Main" transitionName="moveUp">',
+            '  <Location path="/" handler={MainPage} />',
+            '  <Location path="/about" handler={AboutPage} />',
+            '</AnimatedLocations>'
+          ].join('\n')}</pre>
+          <p>
+            Now, click on a link to go to <Link href="/about">about page</Link>.
+          </p>
+        </div>
       </div>
     )
   }
@@ -35,11 +52,34 @@ var AboutPage = React.createClass({
   render: function() {
     return (
       <div className="AboutPage Page">
-        <h1>About</h1>
-        Go to <Link href="/">main page</Link>.
+        <div className="Page__wrapper">
+          <h1>About</h1>
+          <p>Then specify your CSS3 Transition in stylesheet:</p>
+          <pre>{[
+            '.moveUp-enter {',
+            '  ...',
+            '}',
+            '',
+            '.moveUp-enter.moveUp-enter-active {',
+            '  ...',
+            '}',
+            '',
+            '.moveUp-leave {',
+            '  ...',
+            '}',
+            '',
+            '.moveUp-leave.moveUp-leave-active {',
+            '  ...',
+            '}',
+          ].join('\n')}</pre>
+          Go back to <Link href="/">main page</Link>.
+        </div>
       </div>
     )
   }
 })
+
+var rrcHref = "http://andreypopp.viewdocs.io/react-router-component";
+var githubHref = "https://github.com/andreypopp/react-router-page-transition";
 
 React.renderComponent(App(), document.body);
